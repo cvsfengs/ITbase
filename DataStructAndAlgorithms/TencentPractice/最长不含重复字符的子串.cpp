27793a9e7b0dcf5f0d0a9777b3c78937ba283a13
zzpC <1348778498@qq.com>
2018-04-01 23:35:01 +0800

    代码

---
+++ DataStructAndAlgorithms/TencentPractice/最长不含重复字符的子串.cpp
@@ -0,0 +1,42 @@
+// 面试题48：最长不含重复字符的子字符串
+// 题目：请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子
+// 字符串的长度。假设字符串中只包含从'a'到'z'的字符。
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int longestSubstringWithoutDuplication_2(const std::string& str)
+{
+    int curLength = 0;
+    int maxLength = 0;
+
+    int* position = new int[26];
+    for(int i = 0; i < 26; ++i)
+        position[i] = -1;
+
+    for(int i = 0; i < str.length(); ++i)
+    {
+        int prevIndex = position[str[i] - 'a'];
+        if(prevIndex < 0 || i - prevIndex > curLength)
+            ++curLength;
+        else
+        {
+            if(curLength > maxLength)
+                maxLength = curLength;
+
+            curLength = i - prevIndex;
+        }
+        position[str[i] - 'a'] = i;
+    }
+
+    if(curLength > maxLength)
+        maxLength = curLength;
+
+    delete[] position;
+    return maxLength;
+}
+
+int main(){
+	return 0;
+}
